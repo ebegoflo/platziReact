f7c90c779c50ca4e3c492a3c8bfd2f142a4eebb8
RosyG <rmga2106@gmail.com>
2018-04-11 11:32:27 -0500

    Agregando pestañas del filtrado

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,51 @@
 import React from 'react';
-import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 
+// import FontIcon from 'material-ui/FontIcon';
+// import {red500, yellow500, blue500} from 'material-ui/styles/colors';
+//
+// const iconStyles = {
+//   marginRight: 24,
+// };
 
-const TextFieldExampleSimple = () => (
-    <div>
-      <TextField
-        hintText="Hint Text"
-      /><br />
-      <br />
-
-      <Button variant="raised" color="primary">
-        Reproducir
-      </Button>
-
-    </div>
+const button = () => (
+  <div>
+    <Button variant="raised" color="primary">
+      Notas
+    </Button>
+    <Button variant="raised" color="primary">
+      Preguntas
+    </Button>
+    <Button variant="raised" color="primary">
+      Archivos
+    </Button>
+    <Button variant="raised" color="primary">
+      Enlaces
+    </Button>
+    <Button variant="raised" color="primary">
+      Preguntas
+    </Button>
+  </div>
 );
 
-export default TextFieldExampleSimple;
+export default button;
+
+
+// const TextFieldExampleSimple = () => (
+//     <div>
+//       <TextField
+//         hintText="Hint Text"
+//       /><br />
+//       <br />
+//
+//       <Button variant="raised" color="primary">
+//         Reproducir
+//       </Button>
+//
+//     </div>
+// );
+//
+// export default TextFieldExampleSimple;
 
 // import React from 'react';
 // import Button from 'material-ui/Button';
