9f1c994f756ca0e528dc0639fb8dd262a7b72581
RosyG <rmga2106@gmail.com>
2018-04-11 10:46:15 -0500

    width del video

--- src/components/Video.js
+++ src/components/Video.js
@@ -4,7 +4,7 @@ import ReactPlayer from 'react-player';
 
 class Video extends Component  {
   render () {
-    return <ReactPlayer url='https://www.youtube.com/watch?v=-LaY2-nM9kE&t=5s&index=1&list=PLa28R7QEiMblmIF4y_ydG4eRr5K4TcVyk' playing />
+    return <ReactPlayer url='https://www.youtube.com/watch?v=-LaY2-nM9kE&t=5s&index=1&list=PLa28R7QEiMblmIF4y_ydG4eRr5K4TcVyk' width='100%' playing />
 
 
   }
