e33f220dcbb53094a5f30b93113487da1c8c6fe6
ebegoflo <ebenezergf@gmail.com>
2018-04-12 09:11:32 -0500

    Create a React app and publish it to github Pages

---
+++ src/components/Avatar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from 'material-ui/styles';
+import Avatar from 'material-ui/Avatar';
+
+
+
+
